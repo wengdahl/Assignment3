1d5e244bb4460bad9df59913c093df45560508d1
wengdahl <wfengdahl@wpi.edu>
2020-04-16 18:20:31 -0400

    Add employee abstract class

--- src/Employee.java
+++ src/Employee.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
 
-public class Employee {
+public abstract class Employee {
+    private ArrayList<Client> myClients;
+
+    public Employee()
+    {
+        myClients = new ArrayList<Client>();
+    }
+
+    public boolean addClient(Client c)
+    {
+        //Temp, must sort
+        return myClients.add(c);
+    }
+    private ArrayList<Client> getClients()
+    {
+        return myClients;
+    }
 }
